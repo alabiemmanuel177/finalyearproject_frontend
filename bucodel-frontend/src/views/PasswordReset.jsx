7ba8fd7523e8cbb8cfeb793eb96bf7c8862731ae
Alabi <alabiemmanuel177@gmail.com>
2022-10-04 14:03:25 +0100

    finished Password Reset Page

--- bucodel-frontend/src/views/PasswordReset.jsx
+++ bucodel-frontend/src/views/PasswordReset.jsx
@@ -10,7 +10,13 @@ export const PasswordReset = () => {
         <PasswordResetForm />
       </div>
       <div className="logintext">
-        <div className="logintext_container">
+      <div className="login_svg-1">
+          <img
+            src="https://res.cloudinary.com/manlikeemma/image/upload/v1664703033/BUCODEL/Group_1_gc0czt.svg"
+            alt=""
+          />
+        </div>
+        <div className="passrest_text_container">
           <h1>Password Reset</h1>
           <h3>
             Enter your Email and click on the reset button a<br /> link will be
@@ -22,6 +28,12 @@ export const PasswordReset = () => {
         <div className="passwordResetsvg">
           <img
             src="https://res.cloudinary.com/manlikeemma/image/upload/v1659024784/BUCODEL/Password_Reset_zkytni.svg"
+            alt=""
+          />
+        </div>
+        <div className="login_svg-3">
+          <img
+            src="https://res.cloudinary.com/manlikeemma/image/upload/v1664703033/BUCODEL/Group_2_zkqorp.svg"
             alt=""
           />
         </div>
