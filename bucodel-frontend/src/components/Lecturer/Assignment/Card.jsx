bc411669311cfec4bcd2840dbf1217f1511f6fe3
Xavin616 <jacexavinbello@gmail.com>
2023-02-11 12:13:50 +0000

    Bello: Lecturer Gradebooks in Courses tab

--- bucodel-frontend/src/components/Lecturer/Assignment/Card.jsx
+++ bucodel-frontend/src/components/Lecturer/Assignment/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Button, Card, CardActions, CardContent, CardHeader, Grid, Typography } from '@mui/material'
 import { BsRecordCircleFill } from 'react-icons/bs'
+import { NavLink } from 'react-router-dom'
 
 export default function AssignedCards() {
   return (
     <Grid item xs={5} md={3}>
+      <NavLink to='/lecturerdoassignment'>
         <Card>
             <CardHeader
                 sx={{ padding: '10px 20px 10px 10px', position: 'relative' }}
@@ -35,7 +37,8 @@ export default function AssignedCards() {
             <CardActions sx={{padding: '12px 4px'}}>
               <Button sx={{ color: 'gray', textTransform: 'none' }}>Due, Dec 8 •&nbsp; <span style={{color: '#0A3697', fontWeight: 'bold'}}>{' '}8:00pm</span></Button>
             </CardActions>
-        </Card>
+          </Card>
+      </NavLink>
     </Grid>
   )
 }
